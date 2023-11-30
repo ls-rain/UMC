96f971e21f72cc01ace5cf7cfd08f96e8ceadabe
ls-rain <swlee19990115@gmail.com>
2023-11-30 18:17:12 +0900

    ✨ [FEAT] 가게에 리뷰 추가하기 API 구현

---
+++ src/main/java/umc/study/service/StoreService/StoreQueryServiceImpl.java
@@ -0,0 +1,21 @@
+package umc.study.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.study.domain.Store;
+import umc.study.repository.StoreRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StoreQueryServiceImpl implements StoreQueryService{
+    private final StoreRepository storeRepository;
+
+    @Override
+    public Optional<Store> findStore(Long id) {
+        return storeRepository.findById(id);
+    }
+}
